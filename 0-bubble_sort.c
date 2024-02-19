fe4f5ba4aa1ee2ef9700e606858a2d470ef800ad
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-19 05:21:20 -0800

    created the header files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort an array of integers in ascending order
+ * by comparing two element and swaps the smallest to the left.
+ *
+ * @array: the list to sort
+ * @size: size of the array
+ *
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
