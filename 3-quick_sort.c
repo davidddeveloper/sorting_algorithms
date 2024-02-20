3aaf8c25a319214861ce8d655e4aab016c0d693c
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-20 08:43:11 -0800

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,23 +12,44 @@
 
 void quick_sort(int *array, size_t size)
 {
+	int loc;
 	int lo = 0;
 	int hi = size -1;
 
 	if (lo < hi)
 	{
-		
+		loc = partition(array, lo, hi);
+		quick_sort(array, loc);
+		quick_sort(array + loc + 1, size - loc - 1);
 	}
 }
 
 int partition(int array[], int lo, int hi)
 {
-	
+	int pi, start, end;
+
+	start = lo;
+	pi = end = hi - 1;
+
+	while (start < end)
+	{
+		while (start < pi)
+			start++;
+
+		while (end > pi)
+			end--;
+
+		if (start < end)
+			swap(&(array[start]), &(array[end]));
+	}
+
+	swap(&(array[pi]), &(array[end]));
+	return (end);
 }
 
 void swap(int *a, int *b)
 {
-	int *temp = *a;
-	a = *b;
-	b = *temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
