7d25454ac99c4c315a209f15f65c1e7ab653c250
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-20 06:27:15 -0800

    started doing the quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort
+ *
+ * @array: the array to sort
+ * @size: the size of the array
+ *
+ * Return: nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	int lo = 0;
+	int hi = size -1;
+
+	if (lo < hi)
+	{
+		
+	}
+}
+
+int partition(int array[], int lo, int hi)
+{
+	
+}
+
+void swap(int *a, int *b)
+{
+	int *temp = *a;
+	a = *b;
+	b = *temp;
+}
