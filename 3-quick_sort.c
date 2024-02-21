b990df1b3241a3bb11df9886106e47d650c1781d
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-21 04:35:29 -0800

    modified sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,61 +1,67 @@
 #include "sort.h"
-
 /**
- * quick_sort - sorts an array of integers in ascending order
- * using the Quick sort
  *
- * @array: the array to sort
- * @size: the size of the array
  *
- * Return: nothing
+ *
+ *
+ *
+ * /
  */
 
-void quick_sort_helper(int *array, size_t size, const int *original_array)
+void quick_sort_helper(int *array, int low, int hight)
 {
-	int pivot_index;
-	int lo = 0;
-	int hi = size -1;
+	int pi;
 
-	if (size < 2)
-		return;
-
-	pivot_index = partition(array, lo, hi, original_array, size);
-	quick_sort_helper(array, pivot_index - lo, original_array);
-	quick_sort_helper(array + pivot_index + 1, hi - pivot_index, original_array);
+        if (low < hight)
+	{
+		pi = partition(array, low, hight);
+		quick_sort_helper(array, low, pi -1);
+		quick_sort_helper(array, pi + 1, hight);
+	}
 }
 
 void quick_sort(int *array, size_t size)
 {
-	int *original_array = array;
-	quick_sort_helper(array, size, original_array);
-}
+	int low = 0;
+	int hight = (int)(size - 1);
+	int pi;
 
-int partition(int array[], int lo, int hi, const int *original_array, int size)
-{
-	int pivot, i, j;
-
-	pivot = array[hi];
-	i = lo - 1;
-
-	for (j = lo; j <= hi; j++)
+	if (low < hight)
 	{
-		if (array[j] < pivot)
-		{
-			i++;
-			swap(&(array[i]), &(array[j]));
-			print_array(original_array, size);
-		}
+		pi = partition(array, low, hight);
+		quick_sort_helper(array, low, pi - 1);
+		quick_sort_helper(array, pi + 1, hight);
 	}
-
-	swap(&(array[i + 1]), &(array[hi]));
-	print_array(original_array, size);
-
-	return (i + 1);
 }
 
+int partition(int array[], int lo, int hi)
+{
+        int pivot, i, j;
+
+        pivot = array[hi];
+        i = lo - 1;
+
+        for (j = lo; j <= hi; j++)
+        {
+                if (array[j] < pivot)
+                {
+                        i++;
+                        swap(&(array[i]), &(array[j]));
+                }
+        }
+
+        swap(&(array[i + 1]), &(array[hi]));
+
+        return (i + 1);
+}
+/**
+ *
+ *
+ *
+ */
 void swap(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+        int temp = *a;
+        *a = *b;
+        *b = temp;
 }
